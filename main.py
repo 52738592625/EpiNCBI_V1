9147d2730d66ecf34d158dd4bbf1a3bdac821d47
Bryan <bryanharris91@outlook.com>
2025-09-24 14:42:32 -0500

    prerelease

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#Install packages
+#Install packages necessary to run the code
 import subprocess
 subprocess.run(
     f"python -m pip install pandas",
@@ -10,9 +10,15 @@ import pandas as pd
 import numpy as np
 import warnings
 warnings.filterwarnings('ignore')
+
+####################################
+#User selects a pathogen to download
 print("Type the NCBI Taxonomy number of the pathogen you'd like to download, and press enter")
 print("Escherichia coli = 562, Salmonella enterica = 590, etc.")
 choice = input("")
+
+####################################
+#Download step. Filters out sequencing data. Cutoff date is Jan 1, 2024
 print("Downloading. Please wait a while.")
 subprocess.run(
     f"datasets download genome taxon {choice} --include none --released-before 2024-01-01",
@@ -20,6 +26,10 @@ subprocess.run(
     check=True
 )
 print("Download successful")
+####################################
+#Data Formatting Step. Further filters out unused data fields
+
+
 print("Formatting the data. Please wait a while.")
 subprocess.run(
     f"dataformat tsv genome --package ncbi_dataset.zip --fields accession,assminfo-biosample-attribute-name,assminfo-biosample-attribute-value,assminfo-submitter > {choice}_data.tsv",
@@ -29,15 +39,20 @@ subprocess.run(
 print("Data formatting successful")
 if os.path.exists('ncbi_dataset.zip'):
     os.remove('ncbi_dataset.zip')
+####################################
+#Data Pivoting Step
+
+
 print("Cleaning the data. Please wait a while.")
 df = pd.read_csv(f'{choice}_data.tsv', sep='\t')
 df.columns = ['genome', 'attribute', 'attribute_value', 'submitter']
 df = df.pivot_table(index=['genome','submitter'], columns='attribute', values='attribute_value', aggfunc='first').reset_index()
 df.columns.name = None
-#col_interest = ['genome', 'geo_loc_name', 'attribute_package','collected_by', 'collection_date','strain','host','host_age','host_disease','ifsac_category','isolate','isolation_source', 'lat_lon','project_name', 'purpose_of_sampling', 'source_type']
 col_interest = ['genome', 'geo_loc_name','collection_date','strain','host', 'serotype','isolate','isolation_source','source_type', 'submitter']
 df = df[col_interest]
-#df.to_csv(f'{choice}_unstacked.csv', index=False)
+
+####################################
+#Data Cleaning Step
 
 #Create a dictionary from the lookup table from the top US cities and their states
 df_city_state_lookup = pd.read_csv('./lookup_tables/top_us_cities.csv', dtype=str)
@@ -55,10 +70,8 @@ city_state_dict
 for state in city_state_dict:
     city_state_dict[state] = [city.lower() for city in city_state_dict[state]]
     state = state.lower()
-#city_state_dict
 
-#STATE COLUMN WORK
-#1) Create a column called is_usa_based full of NaN values initally
+#Create a column called is_usa_based full of NaN values initally
 df['geo_loc_name'] = df['geo_loc_name'].fillna('')
 df['geo_loc_name_is_missing'] = df['geo_loc_name'] == ''
 df['is_usa_based'] = np.nan
@@ -105,43 +118,10 @@ for abbreviation, state in state_abbreviation_dict.items():
     df.loc[df['geo_loc_name'].str.upper().str.contains(":" + state), 'usa_state'] = state
 df.loc[(df['is_usa_based'] == 1) & (df['usa_state'] == '-'), 'usa_state']  = 'No state'
 df.loc[df['is_usa_based'].isin({0, 98, 99}), 'usa_state'] = 'N/A'
-#remove all rows where is_usa_based is 0 or 99
-df = df[df['is_usa_based'] != 0]
-df = df[df['is_usa_based'] != 99]
-
+#remove all rows where is_usa_based is 0, 98, or 99, leaving only USA based rows
+df = df[df['is_usa_based'] == 1]
 
 df.set_index('genome', inplace=True)
-
-df['new_isolation_source'] = df['isolation_source'].copy()
-df['new_isolation_source'] = df['new_isolation_source'].replace(['missing', 'Unknown', 'Not Provided', 'Not Collected','unknown','not collected'], 'Missing')
-df['new_isolation_source'] = df['new_isolation_source'].replace(['.*chicken.*', '.*Chicken.*'], 'Chicken', regex=True)
-df['new_isolation_source'] = df['new_isolation_source'].replace(['.*pig.*', '.*Pig.*', '.*swine.*', '.*Swine.*', '.*Pork.*', '.*pork.*', '.*bacon.*', '.*Bacon.*', 'Hogs', 'rump swab'], 'Pig', regex=True)
-df['new_isolation_source'] = df['new_isolation_source'].replace(['.*cow.*', '.*Cow.*', '.*bovine.*', '.*Bovine.*', '.*cattle.*', '.*Cattle.*', '.*beef.*', '.*Beef.*', '.*taurus.*'], 'Cow', regex=True)
-df['new_isolation_source'] = df['new_isolation_source'].replace(['.*turkey.*', '.*Turkey.*'], 'Turkey', regex=True)
-df['new_isolation_source'] = df['new_isolation_source'].replace(['.*water.*', '.*Water.*', '.*pond.*', 'sewage'], 'Water', regex=True)
-df['new_isolation_source'] = df['new_isolation_source'].replace(['.*human.*', '.*Human.*'], 'Human', regex=True)
-df['new_isolation_source'] = df['new_isolation_source'].replace(['environmental', 'environment', 'sediment', 'soil', 'environmental swab', 'environmental swab sponge', 'kratom', 'fluff', 'Feedyard Environment', 'farm environment'], 'Environment')
-df['new_isolation_source'] = df['new_isolation_source'].replace(['stool', 'feces', 'urine', 'blood','biological fluid or and tissue', 'poultry manure', 'clinical sample'], 'body_subst_unknown_host')
-df['new_isolation_source'] = df['new_isolation_source'].replace(['intestine','lung','liver','tissue'], 'organ_unknown_host')
-df['new_isolation_source'] = df['new_isolation_source'].replace(['papaya','cucumber','tree nut','peanut butter','brisket swab', 'cilantro', 'Almond kernel','Inshell pistachio from a storage silo', 'food', 'sesame seed', 'egg raw whole'], 'other_food')
-
-df['new_host'] = df['host'].copy()
-df['new_host'] = df['host'].replace(['missing', 'Unknown', 'Not Provided', 'Not Collected','unknown','not collected'], 'Missing')
-df['new_host'] = df['host'].replace(['.*chicken.*', '.*Chicken.*'], 'Chicken', regex=True)
-df['new_host'] = df['host'].replace(['.*pig.*', '.*Pig.*', '.*swine.*', '.*Swine.*', '.*Pork.*', '.*pork.*', '.*bacon.*', '.*Bacon.*', 'Hogs', 'rump swab'], 'Pig', regex=True)
-df['new_host'] = df['host'].replace(['.*cow.*', '.*Cow.*', '.*bovine.*', '.*Bovine.*', '.*cattle.*', '.*Cattle.*', '.*beef.*', '.*Beef.*', '.*taurus.*'], 'Cow', regex=True)
-df['new_host'] = df['host'].replace(['.*turkey.*', '.*Turkey.*'], 'Turkey', regex=True)
-df['new_host'] = df['host'].replace(['.*water.*', '.*Water.*', '.*pond.*', 'sewage'], 'Water', regex=True)
-df['new_host'] = df['host'].replace(['.*human.*', '.*Human.*'], 'Human', regex=True)
-df['new_host'] = df['host'].replace(['environmental', 'environment', 'sediment', 'soil', 'environmental swab', 'environmental swab sponge', 'kratom', 'fluff', 'Feedyard Environment', 'farm environment'], 'Environment')
-df['new_host'] = df['host'].replace(['stool', 'feces', 'urine', 'blood','biological fluid or and tissue', 'poultry manure', 'clinical sample'], 'body_subst_unknown_host')
-df['new_host'] = df['host'].replace(['intestine','lung','liver','tissue'], 'organ_unknown_host')
-df['new_host'] = df['host'].replace(['papaya','cucumber','tree nut','peanut butter','brisket swab', 'cilantro', 'Almond kernel','Inshell pistachio from a storage silo', 'food', 'sesame seed', 'egg raw whole'], 'other_food')
-categories = ['Missing', 'Chicken', 'Pig', 'Cow', 'Turkey', 'Water', 'Human', 'Environment', 'body_subst_unknown_host', 'organ_unknown_host', 'other_food']
-for i in range(len(df)):
-    if df['new_isolation_source'][i] not in categories:
-        df['new_isolation_source'][i] = 'Uncategorized'
-
 df['collection_date'] = df['collection_date'].fillna('missing')
 df['collection_year'] = df['collection_date'].replace(
     ['Missing', 'Unknown', 'unknown', 'none', 'not applicable', 'not collected', 'not provided', 'Not collected', 'MISSING', 'not available', 'Not Collected', 'unspecified', 'Not available'],
@@ -165,12 +145,9 @@ df['collection_year'] = df['collection_date'].apply(extract_year)
 df['month'] = df['collection_date'].apply(lambda x: x[5:7] if len(x) > 5 and x[4] == '-' else None)
 df['month'] = pd.to_numeric(df['month'], errors='coerce')
 
-#replace the entries in df['nors_primary_mode_map'] with the cooresponding values in ncbi_to_nors_source_match.csv
-df_map = pd.read_csv('./lookup_tables/ncbi_to_nors_source_match.csv')
-#rename the columns to isolation_source and Primary Mode
-df_map.columns = ['isolation_source', 'Primary Mode']
-df['nors_primary_mode_map'] = df['isolation_source'].copy()
-df['nors_primary_mode_map'] = df['nors_primary_mode_map'].replace(df_map.set_index('isolation_source')['Primary Mode'].to_dict())
+
+##############################################
+#Saving the data output
 
 df.to_csv(f'./ncbi_{choice}_data.csv', index=True)
 
---
+++ readme.txt
@@ -0,0 +1,19 @@
+This readme documents the procedure for running the code to generate the results shown in "Leveraging NCBI Genomic Metadata for Epidemiological Insights:
+Example of Enterobacterales".
+
+The code that carries out the data pipeline in Figure 1 is detailed in main.py. To run the code, make sure you have Python downloaded. You can download Python at https://www.python.org/downloads/
+Follow the default installation instructions provided for your machine.
+
+Downloading the GitHub repo can be done via cloning the repo through Git, or by direct download of a zip file of the repo, and extracting the contents somewhere on the machine.
+
+The code that carries out the data pipeline in Figure 1 is detailed in main.py. There are many ways
+to run this code. For example, after installing Python on a Windows PC, and extracting a zip download of the repo, navigate to the newly extracted folder
+containing the files. click the search bar at the top of file explorer, type "cmd", and press
+enter to open the command prompt. From the command prompt, type: "python main.py" and press enter.
+The code will automatically install any required Python packages to carry out the functions in the code.
+
+The program will then prompt the user to enter the NCBI Taxonomy ID of the pathogen they'd like to download, and to press enter. IDs can be looked up for individual pathogens on https://www.ncbi.nlm.nih.gov/datasets/
+
+E. Coli's ID is 562, and Salmonella's is 590.
+
+The code will then begin the download, and conclude with cleaning and saving the output data to the same directory main.py resides. Due to the limited bandwidth of NCBI's databases, it may take several minutes for the data to download.
